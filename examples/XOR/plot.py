7d53a17620c8b77a2a3da3cb048e8dc683df8b22
guojj33 <guojj33@mail2.sysu.edu.cn>
2022-09-15 17:10:16 +0800

    update

--- examples/XOR/plot.py
+++ examples/XOR/plot.py
@@ -6,9 +6,15 @@ output_dir = './output/'
 
 train_loss = pd.read_csv(output_dir + 'train_loss.txt', header=None).to_numpy()
 x = range(train_loss.shape[0])
-plt.plot(x, train_loss)
+plt.plot(x, train_loss, )
 title = 'train losses'
 plt.title(title)
 plt.xlabel("epoch")
 plt.ylabel("BCE loss")
+
+# train_loss = pd.read_csv(output_dir + 'train_loss not learnable.txt', header=None).to_numpy()
+# plt.plot(x, train_loss, c='r')
+
+# plt.legend(('learnable', 'not learnable'))
+
 plt.savefig(output_dir + title + '.png')
